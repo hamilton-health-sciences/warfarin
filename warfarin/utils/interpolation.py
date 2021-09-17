64bd69a2b5484f26072d38bebe0ccf946bdad884
Walter Nelson <nelsonwa@hhsc.ca>
2021-09-17 16:08:53 -0400

    Fix interpolation bug
    Fixes #64

--- warfarin/utils/interpolation.py
+++ warfarin/utils/interpolation.py
@@ -2,6 +2,19 @@ import numpy as np
 
 
 def interpolate_inr(df):
+    """
+    Linearly interpolate INR values on unobserved study days.
+
+    Args:
+        df: A DataFrame indexed by TRIAL, SUBJID, TRAJID, and STUDY_DAY, and
+            containing a column INR_VALUE.
+
+    Returns:
+        df_interp: A DataFrame exploded to be indexed TRIAL, SUBJID, TRAJID, and
+                   STUDY_DAY where STUDY_DAY now can assume all possible
+                   intermediate values as well. INR_VALUE will be linearly
+                   interpolated on those intermediate study days.
+    """
     days = df.reset_index().groupby(
         ["TRIAL", "SUBJID", "TRAJID"]
     )["STUDY_DAY"].min().to_frame().rename(
@@ -23,11 +36,9 @@ def interpolate_inr(df):
         ["TRIAL", "SUBJID", "TRAJID", "STUDY_DAY"]
     ).join(df)
 
-    # Linearly interpolate INR. We don't need a groupby here because INRs are
-    # always observed at the start and end of a trajectory.
-    df_interp["INR_VALUE"] = df_interp["INR_VALUE"].interpolate()
+    # Linearly interpolate INR
+    df_interp["INR_VALUE"] = df_interp.groupby(
+        ["TRIAL", "SUBJID", "TRAJID"]
+    )["INR_VALUE"].interpolate()
 
     return df_interp
-
-
-
