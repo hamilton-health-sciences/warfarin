5d12b60532df677cce928abd3e54cae6f3bc45c2
Walter Nelson <nelsonwa@hhsc.ca>
2021-10-04 13:40:02 -0400

    Bug fixes to evaluation script

--- scripts/evaluate_best_model.py
+++ scripts/evaluate_best_model.py
@@ -1,3 +1,5 @@
+"""Evaluate the reinforcement learning model."""
+
 import os
 
 from warnings import warn
@@ -11,26 +13,26 @@ from ray.tune import Analysis
 from plotnine.exceptions import PlotnineError
 
 from warfarin import config
-from warfarin.utils.smdp_buffer import SMDPReplayBuffer
-from warfarin.models.smdp_dBCQ import discrete_BCQ
+from warfarin.models import SMDBCQ
+from warfarin.utils.modeling import get_dataloader
 from warfarin.evaluation import evaluate_and_plot_policy
 
 
 def main(args):
-    # TODO donot commit this!!!
-    config.MIN_TRAINING_EPOCHS = 50
     # Load up the metrics for models trained as part of the grid search
     analysis = Analysis(args.logs_path)
     dfs = analysis.trial_dataframes
 
+    # Select the best model by name and iteration number (useful for sanity
+    # checks)
     if args.trial_name_filter and args.step_idx:
         best_trial_name = [
             k for k in dfs if args.trial_name_filter in k
         ][0]
         best_trial_iter = args.step_idx
+    # Or more likely, select the best model by optimizing a given metric across
+    # the hyperparameter sweep
     else:
-        # TODO remove this line:
-        dfs = {k: df for k, df in dfs.items() if args.target_metric in df}
         # Get best model according to the chosen metric
         if args.mode == "max":
             max_metric = lambda k: dfs[k].loc[
@@ -60,50 +62,40 @@ def main(args):
     # Load the checkpointed model for evaluation
     trial_config = analysis.get_all_configs()[best_trial_name]
 
-    # TODO probably refactor this as it's duplicated from the tuning script rn
-    # Data dimensionality from buffers
-    num_actions = 7  # TODO
-    state_dim = 56  # ...
+    # Load the data
+    if "test" in args.data_path:
+        raise ValueError("We're not testing on the test set yet.")
+    data, _ = get_dataloader(
+        data_path=args.data_path,
+        cache_name="train_buffer.pkl",
+        batch_size=trial_config["batch_size"],
+        discount_factor=trial_config["discount"]
+    )
 
-    # Build the policy object
-    policy = discrete_BCQ(
-        num_actions,
-        state_dim,
-        "cuda",
-        trial_config["bcq_threshold"],
-        trial_config["discount"],
-        trial_config["optimizer"],
-        {"lr": trial_config["learning_rate"]},
-        trial_config["polyak_target_update"],
-        trial_config["target_update_freq"],
-        trial_config["tau"],
-        0.1,
-        0.1,
-        1,
-        0.,
-        trial_config["hidden_dim"],
-        trial_config["num_layers"]
+    # TODO probably refactor this as it's duplicated from the tuning script rn
+    # Construct the model and load it
+    num_actions = len(config.ACTION_LABELS)
+    policy = SMDBCQ(
+        num_actions=num_actions,
+        state_dim=data.state_dim,
+        device="cuda",
+        BCQ_threshold=trial_config["bcq_threshold"],
+        discount=trial_config["discount"],
+        optimizer=trial_config["optimizer"],
+        optimizer_parameters={"lr": trial_config["learning_rate"]},
+        polyak_target_update=trial_config["polyak_target_update"],
+        target_update_frequency=trial_config["target_update_freq"],
+        tau=trial_config["tau"],
+        hidden_states=trial_config["hidden_dim"],
+        num_layers=trial_config["num_layers"]
     )
     state_dict_path = os.path.join(best_trial_name,
                                    f"checkpoint_{best_trial_iter:06d}",
                                    "model.pt")
-    state = torch.load(state_dict_path)
-    policy.Q.load_state_dict(state)
-
-    # TODO don't hardcode device
-    # Load the buffer we're evaluating on
-    if "test" in args.buffer_path:
-        raise ValueError("We're not testing on the test set yet.")
-    buf = SMDPReplayBuffer.from_filename(
-        data_path=args.buffer_path,
-        batch_size=trial_config["batch_size"],
-        buffer_size=1e7,
-        device="cuda"
-    )
-    buf.max_size = len(buf.data)
+    policy.load(state_dict_path)
 
     # Compute evaluation metrics on the buffer
-    metrics, plots, _ = evaluate_and_plot_policy(policy, buf)
+    metrics, plots, _ = evaluate_and_plot_policy(policy, data)
 
     # Create output directories
     plots_dir = os.path.join(args.output_prefix, "plots")
@@ -119,6 +111,7 @@ def main(args):
 
     # Output plots
     for plot_name, plot in plots.items():
+        plot_name = plot_name.replace("/", "_")
         print(f"Saving {plot_name}...")
         plot_fn = os.path.join(plots_dir, f"{plot_name}.jpg")
         try:
@@ -152,10 +145,10 @@ if __name__ == "__main__":
               "evaluation metric")
     )
     parser.add_argument(
-        "--buffer_path",
+        "--data_path",
         type=str,
         required=True,
-        help="Path to the buffer to evaluate on"
+        help="Path to the data to evaluate on"
     )
     parser.add_argument(
         "--target_metric",
