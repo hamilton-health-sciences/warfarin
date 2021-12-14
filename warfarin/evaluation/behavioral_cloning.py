19a8ac78500b6fb6e288f60131e381d4fc8655f7
Walter Nelson <nelsonwa@hhsc.ca>
2021-12-14 14:17:39 -0500

    Add additional metrics to behavior cloner eval
    Add the following metrics at a given BCQ threshold (typically set to 0.3
    or 0.2 in our experiments):
    * Coverage: Proportion of instances where the observed option is in the
      feasible set.
    * Consistency: Proportion of instances where the feasible options are
      ordinally contiguous.
    * Multi-output F1: F1 score in the "multioutput" problem where the
      feasible set is the output and the true option is the true output.
    
    Also add the following probability calibration metric, useful from the
    perspective of WIS:
    * Calibration error: The expected calibration error for the multi-class
      case.
    
    Fixes #94, fixes #95

--- warfarin/evaluation/behavioral_cloning.py
+++ warfarin/evaluation/behavioral_cloning.py
@@ -7,9 +7,126 @@ import scipy.stats
 
 import pandas as pd
 
-from sklearn.metrics import roc_auc_score, f1_score
+from sklearn.metrics import roc_auc_score, f1_score, classification_report
 
 from warfarin.evaluation.plotting import plot_policy_heatmap
+
+
+def coverage(prob, y, thresh=0.3):
+    """
+    Computes the fraction of instances where the observed option is included in
+    the feasible set at a given BCQ threshold.
+
+    Args:
+        prob: The probability outputs of the model.
+        y: The observed option.
+        thresh: The BCQ threshold.
+
+    Returns:
+        coverage: The fraction of instances where the observed option is
+                  included in the feasible set.
+    """
+    covered = (prob.T / prob.max(axis=1)).T > thresh
+    transition_idx = np.arange(prob.shape[0]).astype(int)
+    coverage = covered[transition_idx, y].sum() / prob.shape[0]
+
+    return coverage
+
+
+def multi_output_f1_score(prob, y, thresh=0.3):
+    """
+    Computes the multi-output F1 score at a given BCQ threshold.
+
+    Args:
+        prob: The probability outputs of the model.
+        y: The observed option.
+        thresh: The BCQ threshold.
+
+    Returns:
+        f1: The multi-output F1 score.
+    """
+    covered = (prob.T / prob.max(axis=1)).T > thresh
+    f1 = classification_report(
+        np.array(pd.get_dummies(pd.Categorical(y))),
+        (prob.T / prob.max(axis=1)).T > thresh,
+        output_dict=True
+    )["macro avg"]["f1-score"]
+
+    return f1
+
+
+def consistency(prob, thresh=0.3):
+    """
+    Computes the fraction of instances where the feasible set is consistent.
+
+    Args:
+        prob: The probability outputs of the model.
+        thresh: The BCQ threshold.
+
+    Returns:
+        consistency: The fraction of instances where the feasible options are
+                     contiguous in ordinal option space.
+    """
+    covered = ((prob.T / prob.max(axis=1)).T > thresh).astype(float)
+    D = np.diff(covered, axis=-1)
+
+    # Impute leading zeros
+    idxs = (D != 0.).argmax(axis=-1)
+    for n, idx in enumerate(idxs):
+        D[n, :idx] = 2.
+    # Impute trailing zeros
+    idxs = D.shape[1] - (np.flip(D, axis=-1) != 0.).argmax(axis=-1)
+    for n, idx in enumerate(idxs):
+        D[n, idx:] = -2.
+
+    c = np.sign(-D)
+    c_all = np.all(np.sort(c, axis=-1) == c, axis=-1)
+    c_single = np.sum(c != 0, axis=-1) == 1
+
+    consistency = (c_all | c_single).sum() / prob.shape[0]
+
+    return consistency
+
+
+def calibration_error(yprob, y, binwidth=0.1):
+    """
+    Returns the expected calibration error of the model, as defined in [1].
+
+    [1] Guo et al., On Calibration of Modern Neural Networks.
+        https://arxiv.org/pdf/1706.04599.pdf
+
+    Args:
+        yprob: The probability outputs of the model.
+        y: The observed option.
+        binwidth: The width of the bins to cosnider. Empirically, in our case it
+                  is not particularly sensitive to this parameter.
+
+    Returns:
+        ece: The expected calibration error.
+    """
+    _df = pd.DataFrame(yprob)
+    _df["y"] = y
+    _df = _df.melt(id_vars=["y"])
+    _df.columns = ["y", "ypred", "yprob"]
+
+    _df["correct"] = (_df["y"] == _df["ypred"])
+
+    _df["yprob_bin"] = pd.cut(_df["yprob"],
+                              np.arange(0., 1. + binwidth, binwidth).round(decimals=2),
+                              labels=np.arange(0., 1., binwidth) + binwidth / 2.)
+
+    comp_df = _df.groupby(["ypred", "yprob_bin"])["correct"].mean().to_frame()
+    comp_df["n"] = _df.groupby(["ypred", "yprob_bin"])["correct"].count()
+    comp_df = comp_df.reset_index()
+    comp_df["ypred"] = pd.Categorical(comp_df["ypred"], ordered=True)
+    comp_df["yprob_bin"] = np.array(comp_df["yprob_bin"])
+
+    ece = (
+        np.abs(comp_df["yprob_bin"] -
+               comp_df["correct"]) * comp_df["n"] / comp_df["n"].sum()
+    ).sum()
+
+    return ece
 
 
 def evaluate_behavioral_cloning(model, data):
@@ -35,9 +152,9 @@ def evaluate_behavioral_cloning(model, data):
     y = option.squeeze()
     ypred = yprob.argmax(dim=1)
 
-    ypred_ary = ypred.cpu().detach()
-    y_ary = y.cpu().detach()
-    yprob_ary = yprob.cpu().detach()
+    ypred_ary = ypred.cpu().detach().numpy()
+    y_ary = y.cpu().detach().numpy().astype(int)
+    yprob_ary = yprob.cpu().detach().numpy()
 
     # Accuracy, AUROC, F1 score, and Spearman rank-correlation coefficient
     acc = (ypred == y).sum() / len(y)
@@ -48,7 +165,15 @@ def evaluate_behavioral_cloning(model, data):
     metrics = {"accuracy": acc.item(),
                "auroc": auroc,
                "f1": f1,
-               "rank_corr": corr}
+               "rank_corr": corr,
+               "bcq_coverage_0.2": coverage(yprob_ary, y_ary, 0.2),
+               "bcq_coverage_0.3": coverage(yprob_ary, y_ary, 0.3),
+               "multi_f1_0.2": multi_output_f1_score(yprob_ary, y_ary, 0.2),
+               "multi_f1_0.3": multi_output_f1_score(yprob_ary, y_ary, 0.3),
+               "consistency_0.2": consistency(yprob_ary, 0.2),
+               "consistency_0.3": consistency(yprob_ary, 0.3),
+               "calibration_error": calibration_error(yprob_ary, y_ary),
+               "calibration_error": calibration_error(yprob_ary, y_ary)}
 
     # Plotting
     plot_df = pd.DataFrame(
