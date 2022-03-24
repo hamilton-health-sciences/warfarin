b70c208eb71341bdfe224dd8dc4ce5cc4461327d
Walter Nelson <nelsonwa@hhsc.ca>
2022-03-24 14:26:10 -0400

    Use the original RE-LY algorithm
    This algorithm always provides a dosing decsion, making it more
    convenient to work with (and a better reflection of earlier work).
    
    Fixes #120

--- warfarin/models/baselines/threshold_model.py
+++ warfarin/models/baselines/threshold_model.py
@@ -8,16 +8,15 @@ class ThresholdModel:
     Implements the warfarin-dosing threshold model given in [1].
 
     Specifically, it's a rule-based approach:
-        * If the INR is less than 1 or greater than 4, do not make a decision.
-        * If the INR is between 1 and 1.5, increase the dose by 15%.
+        * If the INR is less than 1.5, increase the dose by 15%.
         * If the INR is between 1.5 and 2, increase the dose by 10%.
-        * If the INR is greater than 3 twice in a row, decrease the dose by 10%.
+        * If the INR is between 3 and 5, then decrease the dose by 10%.
+        * If the INR is greater than 5, then decrease the dose by 15%.
         * Otherwise, maintain the dose.
 
-    [1] Nieuwlaat, R. et al. Randomised comparison of a simple warfarin dosing
-        algorithm  versus a computerised anticoagulation management system for
-        control of warfarin maintenance therapy. Thromb Haemost, 2012.
+    [1] doi:10.1161/CIRCULATIONAHA.112.101808
     """
+
     def select_action(self, previous_inr, current_inr):
         """
         Give the threshold algorithm's action.
@@ -32,15 +31,13 @@ class ThresholdModel:
             model_actions: The decision of the algorithm based on the rules.
         """
         conditions = [
-            (current_inr < 1.) | (current_inr > 4.),
-            current_inr < 1.5,
+            current_inr <= 1.5,
             current_inr < 2.,
             (current_inr >= 2.) & (current_inr <= 3.),
-            (current_inr > 3.) & (np.isnan(previous_inr) |
-                                  (previous_inr <= 3.)),
-            (current_inr > 3.) & (previous_inr > 3.)
+            (current_inr > 3.) & (current_inr < 5),
+            current_inr >= 5.
         ]
-        actions = [np.nan, 5, 4, 3, 3, 2]
+        actions = [5, 4, 3, 2, 1]
         model_actions = np.select(conditions, actions)
 
         return model_actions
@@ -57,15 +54,13 @@ class ThresholdModel:
             model_actions: The decision of the algorithm based on the rules.
         """
         conditions = [
-            (current_inr < 1.) | (current_inr > 4.),
-            current_inr < 1.5,
+            current_inr <= 1.5,
             current_inr < 2.,
             (current_inr >= 2.) & (current_inr <= 3.),
-            (current_inr > 3.) & (np.isnan(previous_inr) |
-                                  (previous_inr <= 3.)),
-            (current_inr > 3.) & (previous_inr > 3.)
+            (current_inr > 3.) & (current_inr < 5),
+            current_inr >= 5.
         ]
-        actions = [np.nan, 0.15, 0.1, 0., 0., -0.1]
+        actions = [0.15, 0.1, 0., -0.1, -0.15]
         model_actions = np.select(conditions, actions)
 
         # Convert to relative dose changes
